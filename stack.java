6b12a5e50c1e264c08a2086dfd640779b9a76e08
Chinmay-P-Patil <codingfriend1111@gmail.com>
2025-04-10 00:59:57 +0530

    stack

---
+++ stack.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class stack{
+    public static void main(String[] args){
+        Stack<String> stack = new Stack<String>();
+        System.out.println(stack.empty());
+    }
+}
